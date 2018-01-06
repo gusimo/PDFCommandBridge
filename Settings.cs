714bd35398be1f7e1884d971204cacc621132e06
Guido Simon <memondia@outlook.de>
2018-01-06 14:10:01 +0100

    Committed initial version

---
+++ Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PdfCommandBridge
+{
+    public class Settings
+    {
+        public bool testmode { get; set;}
+        public string command { get; set; }
+    }
+}
